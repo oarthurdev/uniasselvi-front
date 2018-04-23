7870e13e6ca878461a6447132407c621e065ce6b
Arthur Wagenknecht <arthur_2@live.com>
2018-04-23 15:53:47 -0300

    Alteração na Home

--- src/examples/Dashboard.v1.vue
+++ src/examples/Dashboard.v1.vue
@@ -1,54 +1,41 @@
 <template>
   <div class="dashboard">
     <row>
-      <div class="col-lg-3 col-xs-6">
-        <va-small-box
-          color="aqua"
-          icon="ion-bag"
-          title="150"
-          description="New Order"
-          @more-click="clickToSmallBox"
-          moreText="More info">
-        </va-small-box>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-        <va-small-box
-          color="green"
-          icon="ion-stats-bars"
-          title="53%"
-          description="Bounce Rate"
-          moreText="More info">
-        </va-small-box>
-      </div>
-      <div class="col-lg-3 col-xs-6">
+      <div class="col-md-4 col-sm-6 col-xs-12">
         <va-small-box
           color="yellow"
           icon="ion-person-add"
           title="44"
-          description="User Registrations"
+          description="Contas Registradas"
           moreText="More info">
         </va-small-box>
       </div>
-      <div class="col-lg-3 col-xs-6">
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <va-small-box
+          color="green"
+          icon="ion-person-add"
+          title="44"
+          description="GMs Cadastrados"
+          moreText="More info">
+        </va-small-box>
+      </div>
+      <div class="col-md-4 col-sm-6 col-xs-12">
         <va-small-box
           color="red"
-          icon="ion-pie-graph"
-          title="65"
-          description="Unique Visitors"
+          icon="ion-person"
+          title="44"
+          description="Usuários Banidos"
           moreText="More info">
         </va-small-box>
       </div>
     </row>
 
     <row>
-      <section class="col-lg-7 connectedSortable ui-sortable">
-        <va-chat-box></va-chat-box>
-        <va-todo-list></va-todo-list>
-        <va-quick-mail></va-quick-mail>
-      </section>
-      <section class="col-lg-5 connectedSortable ui-sortable">
-        <va-calendar></va-calendar>
-      </section>
+      <div class="col-md-12 col-sm-6 col-xs-12">
+        <section class="connectedSortable ui-sortable">
+          <usuarios-registrados></usuarios-registrados>
+        </section>
+      </div>
     </row>
   </div>
 </template>
@@ -59,6 +46,7 @@ import VAChatBox from '../widgets/VAChatBox.vue'
 import VATodoList from '../widgets/VATodoList.vue'
 import VAQuickMail from '../widgets/VAQuickMail.vue'
 import VACalendar from '../widgets/VACalendar.vue'
+import UsuariosRegistrados from '../widgets/UsuariosRegistrados.vue'
 
 export default {
   name: 'dashboard',
@@ -77,7 +65,8 @@ export default {
     'va-chat-box': VAChatBox,
     'va-todo-list': VATodoList,
     'va-quick-mail': VAQuickMail,
-    'va-calendar': VACalendar
+    'va-calendar': VACalendar,
+    'usuarios-registrados': UsuariosRegistrados
   }
 }
 
---
+++ src/widgets/UsuariosRegistrados.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box box-primary" data-widget="box-widget">
+    <div class="box-header">
+      <h3 class="box-title">Ultimos usuários</h3>
+        <div class="box-tools pull-right">
+          <span class="label label-danger">1 Novo Usuário</span>
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+        </div>
+    </div>
+  <div class="box-body">
+  </div>
+</div>
+</template>
